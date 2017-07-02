bdef991b5032faef57493a5a82fe7fd0b46c4416
yusuf-ardiansyah <yusuf-ardiansyah@users.noreply.github.com>
2017-07-03 02:48:27 +0700

    Update install-sqlmap.sh

--- install-sqlmap.sh
+++ install-sqlmap.sh
@@ -33,7 +33,7 @@ fi
 #if [[ $vps = "zvur" ]]; then
 	#source="http://"
 #else
-	source="https://raw.githubusercontent.com/yusuf-ardiansyah/y"
+	source="https://raw.githubusercontent.com/yusuf-ardiansyah/x"
 #fi
 
 # go to root
